39aadf27627df7b6eb033fc4e433423e34ed50d1
hyeyoonc <lucy8624@naver.com>
2020-09-06 18:54:30 +0900

    solveProblem

---
+++ Q_사칙연산_10171.java
@@ -0,0 +1,12 @@
+package ProblemYeah;
+
+public class Q_사칙연산_10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
