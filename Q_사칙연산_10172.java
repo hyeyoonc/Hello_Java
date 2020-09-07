5806f90dd30e7210ea1986ccbf58e711b2727341
hyeyoonc <lucy8624@naver.com>
2020-09-07 21:15:07 +0900

    solveProblem

---
+++ Q_사칙연산_10172.java
@@ -0,0 +1,14 @@
+package ProblemYeah;
+
+public class Q_사칙연산_10172 {
+
+	public static void main(String[] args) {
+			System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+			System.out.println("( 0 )\"\"\"\\");
+			System.out.println("|\"^\"`    |");
+			System.out.println("||_/=\\\\__|");
+			
+	}
+
+}
