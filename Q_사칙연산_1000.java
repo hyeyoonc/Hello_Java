bc1d16c23a6c3f6b49bc84d746969bbc23255e03
hyeyoonc <lucy8624@naver.com>
2020-09-10 18:32:41 +0900

    solveproblem

---
+++ Q_사칙연산_1000.java
@@ -0,0 +1,17 @@
+package ProblemYeah;
+
+import java.util.Scanner;
+
+public class Q_사칙연산_1000 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		
+		int b = scan.nextInt();
+		
+		System.out.println(a + b);
+		
+	}
+
+}
