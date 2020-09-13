aa4977b64b1738556e6927164646eb176e4df642
hyeyoonc <lucy8624@naver.com>
2020-09-13 13:09:59 +0900

    solveproblem

---
+++ Q_사칙연산_10430.java
@@ -0,0 +1,22 @@
+package ProblemYeah;
+
+import java.util.Scanner;
+
+public class Q_사칙연산_10430 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		System.out.println((a+b) % c);
+		System.out.println(((a%c) + (b%c))% c);
+		System.out.println((a*b) % c);
+		System.out.println(((a%c) * (b%c))% c);
+		
+	}
+
+}
